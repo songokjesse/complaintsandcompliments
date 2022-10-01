dc2b5c896b865b5eb5efb19e2b463e5c36642568
Jesse Songok <jessesongok@gmail.com>
2022-10-01 20:43:58 +0300

    Feedback Form -> fix submit

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ export default function Home() {
               </label>
               <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  name="phone" type="email" placeholder="Email"/>
+                  name="email" type="email" placeholder="Email"/>
             </div>
             <div className="mb-6">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
